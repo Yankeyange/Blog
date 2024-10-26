efe05c53b1d997f6ff65f1789e6f9069fbd86dfc
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-10-26 00:19:18 +0000

    Add files via upload

--- projet/blog/views.py
+++ projet/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from blog.models import Topic
+from blog.models import Topic, Entry
 from blog.forms import TopicForm, EntryForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # notre home page
@@ -16,6 +17,8 @@ def topics(request):
 # pour afficher les éléments d'un élément
 def topic(request, topic_id):
   topic = Topic.objects.get(id=topic_id)
+  # je met ce élément parce que dans mon modèles j'ai signifie 
+  # le related_name = "entries"
   entries = topic.entries.all()
   context = {'topic':topic, 'entries': entries}
   return render(request, 'topic.html', context)
@@ -25,13 +28,16 @@ def topic(request, topic_id):
 def new_topic(request):
 
   if request.method != 'POST':
+    # form va iriter de TopicForm
     form = TopicForm()
   
   else:
     form = TopicForm(data=request.POST)
 
     if form.is_valid():
+      # si le formulaire est valid on sauvegarde
       form.save()
+      # et on retourne à topics
       return redirect('topics')
   context = {'form':form}
   return render(request, 'new_topic.html', context)
@@ -44,6 +50,7 @@ def new_entry(request, topic_id):
   if request.method != "POST":
     form = EntryForm()
   else:
+    # permet d'accéder aux données envoyer par une requête post
     form = EntryForm(data=request.POST)
     if form.is_valid():
       new_entry = form.save(commit=False)
@@ -53,3 +60,44 @@ def new_entry(request, topic_id):
   context = {'topic': topic, 'form':form}
   return render(request, 'new_entry.html', context)
 
+
+# ou l'utilisateur pourra modifier ces entrées
+
+def edit_entry(request, entry_id):
+  """ on pourra modifier une entré existante"""
+  entry = Entry.objects.get(id=entry_id)
+  # on récupère le sujet topic de cette entitée
+  topic = entry.topic
+
+  if request.method !="POST":
+    # cette partie signifie que j'initialise mon formulaire avec
+    # l'instance entry déjà crée
+    # cela permettra à l'utilisateur de faire les modifications
+    form = EntryForm(instance=entry)
+
+  else:
+    form = EntryForm(instance=entry, data=request.POST)
+    if form.is_valid():
+      form.save()
+      # cela permet de retourner l'utilisateur à la page de topic là
+      # mais avec une action réussi 
+      return redirect("topic", topic_id=topic.id)
+    
+  context = {'entry':entry, 'topic':topic, 'form':form}
+  return render(request, 'edit_entry.html', context)
+
+# login page 
+def loginpage(request):
+   if request.method =="POST":
+        name=request.POST.get("username")
+        pass1=request.POST.get("pass")
+        print(name, pass1)
+
+# logout page 
+def logoutpage(request):
+  pass
+
+# registration page
+
+def registration(request):
+  pass
